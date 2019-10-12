ac605e21fc84da2c542259888263759750f7b0a6
cai <shashrvacai@users.noreply.github.com>
2019-10-11 18:00:00 -0700

    added 3 more images

--- moving slit/sketch.js
+++ moving slit/sketch.js
@@ -1,14 +1,17 @@
-let img;
+let img,img1,img3;
 let a = 0,
 	w = 1;
 var can1, can2, can3;
 
 function preload() {
-	img = loadImage('fish.jpg');
+	img = loadImage('03.jpg');
+	img1 = loadImage('bird.jpg');
+	img2 = loadImage('fish.jpg');
+	img3 = loadImage('02.jpg');
 }
 
 function setup() {
-	can1 = createCanvas(750, 750);
+	can1 = createCanvas(750, 3000);
 
 	//	can2 = createCanvas(750, 750);
 	w = width / 500;
@@ -17,7 +20,11 @@ function setup() {
 
 function draw() {
 	background(255);
-	image(img, 0, 0, width, height);
+	image(img, 0, 0, width, 750);
+	image(img1, 0, 0, width, 750);
+	image(img2, 0, 0, width, 750);
+	image(img3, 0, 0, width, 750);
+	
 	let x = sin(a) * width / 8 - 11.5;
 
 	fill(0);
